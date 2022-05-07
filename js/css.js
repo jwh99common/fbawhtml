afc36c57660bad3b5515c29f669992901e87361a
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-07 13:12:34 +0100

    Update css.js

--- js/css.js
+++ js/css.js
@@ -2,6 +2,9 @@ function changecss () {
   alert ('changecss: entry');
 
   document.getElementById('header').style.fontSize='35px';
+  
+  alert ('changecss: middle');
+  
   document.getElementById('header1').style.fontSize='70px';
   
   alert ('changecss: exit');
