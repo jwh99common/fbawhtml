9eb6b1303cedf969133962d3fe7b0d2a4c81fcf9
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-07 13:16:15 +0100

    Update css.js

--- js/css.js
+++ js/css.js
@@ -2,10 +2,12 @@ function changecss () {
   alert ('changecss: entry');
 
   document.getElementById('header').style.fontSize='35px';
+  document.getElementById('header').style.textAlign ='center';
   
   alert ('changecss: middle');
   
   document.getElementById('header1').style.fontSize='70px';
+  document.getElementById('header1').style.textAlign ='right';
   
   alert ('changecss: exit');
 
