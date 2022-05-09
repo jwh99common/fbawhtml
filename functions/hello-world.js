18eaca5dc7549fe9411b401c2c5ff786f46cddce
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-09 10:13:01 +0100

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,4 +1,15 @@
 
-export  function myfunctionX(a,b) {
+export  function myfunctionX (context) {
+  
+   // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+  
   return  'Hello World';
    };
