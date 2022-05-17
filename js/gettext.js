01582442db5f1eac636c1f109e28a723e4446f9b
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-17 15:36:44 +0100

    Update gettext.js

--- js/gettext.js
+++ js/gettext.js
@@ -1,7 +1,10 @@
 async function getText ()  {
 	const response = await fetch('https://falling-sea-d98a.john-hardie.workers.dev');
+	
   	const text = await response.text();
+	
 	console.log (text);
+	
 	return (text);
 }
 
