eb3bf6dffc3f5c78816aafef2fa386a8740f78f9
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-22 15:34:18 +0100

    Update json.js

--- js/json.js
+++ js/json.js
@@ -43,17 +43,3 @@ function show(data) {
 	
 }
 
-function updateJson () {
-
-	set fso = CreateObject("Scripting.FileSystemObject"); 
-    	set s   = fso.CreateTextFile("json/filename.txt", True);	
-	
-	var firstName = document.getElementById('FirstName');
-    	var lastName  = document.getElementById('lastName');
- 
-    	s.writeline("First Name :" + FirstName);
-    	s.writeline("Last Name :" + lastName);
-     	s.writeline("-----------------------------");
-    	s.Close();
-  
-}
