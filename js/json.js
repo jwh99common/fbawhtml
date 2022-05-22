ffcc65ee3090f5119dfa7a0cba5f4a2e6c412078
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-22 07:37:08 +0100

    Update json.js

--- js/json.js
+++ js/json.js
@@ -2,29 +2,28 @@
 const api_url = "json/employees.json";
 
 // Defining async function
+
 async function getapi(url) {
 	
-	console.log(url);
 	// Storing response
 	const response = await fetch(url);
 	
 	// Storing data in form of JSON
 	var data = await response.json();
-	console.log ('b');
 	
-	console.log(data);
 	
 	show(data);
-	console.log ('c');
 	
 }
 
 // Calling that async function
+
 console.log (api_url);
 getapi(api_url);
 
 
 // Function to define innerHTML for HTML table
+
 function show(data) {
 	 let tab = 
         `<tr>
@@ -34,20 +33,13 @@ function show(data) {
           <th>Salary</th>
          </tr>`;
 	
-	
-	console.log ('c')
-	
 	for(var i = 0; i < data.length; i++) {
 		var r = data[i]; 
 		tab += `<tr> 
     		<td>${r.name} </td>
-    		<td>${r.age}</td>
-    		<td>${r.position}</td> 
-    		<td>${r.salary}</td>    
+    		<td>${r.age}</td>   		  		
 		</tr>`;
-    		console.log(r.name + r.age);
-	}
-	console.log ('d');
+    		}
 	document.getElementById("employees").innerHTML = tab;
 	
 }
