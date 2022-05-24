51cc6eb7ebb37331fc7af363f054cc940e938f5a
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-24 10:02:26 +0100

    Update hw.js

--- functions/hw.js
+++ functions/hw.js
@@ -1,6 +1,21 @@
-// Reacts to POST /hello-world
+/* Reacts to POST /hello-world
 export async function onRequestPost(request) {
   // ...
   console.log ('a');
   return (`Hello world`);
+}
+*/
+
+export async function onRequest(context) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+
+  return new Response("Hello, world!");
 }
