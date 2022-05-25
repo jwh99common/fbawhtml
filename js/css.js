6d609e01c2c302fe498456c1fcfb99a87fca1297
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-25 10:06:31 +0100

    Update css.js

--- js/css.js
+++ js/css.js
@@ -2,11 +2,8 @@ function changecss () {
 
   document.getElementById('header').style.fontSize='100px';
   document.getElementById('header').style.textAlign ="left";
+  document.getElementById('header2').style.fontSize='70px';
+  document.getElementById('header2').style.textAlign ="right";
   
-  
-  document.getElementById('header1').style.fontSize='70px';
-  document.getElementById('header1').style.textAlign ="right";
-  
-return ('hello world');
   
 }
