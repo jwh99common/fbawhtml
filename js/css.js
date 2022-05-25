aa9165ca4026b4036f2368dafe3d444e6e23ce48
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-25 10:10:03 +0100

    Update css.js

--- js/css.js
+++ js/css.js
@@ -1,7 +1,7 @@
 function changecss () {
 
-  document.getElementById('header').style.fontSize='100px';
-  document.getElementById('header').style.textAlign ="left";
+  document.getElementById('header1').style.fontSize='100px';
+  document.getElementById('header1').style.textAlign ="left";
   document.getElementById('header2').style.fontSize='70px';
   document.getElementById('header2').style.textAlign ="right";
   
