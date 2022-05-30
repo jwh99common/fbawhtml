092be03f096f60ee7102f8c46e73357b40fc4bec
jwh99common <103562837+jwh99common@users.noreply.github.com>
2022-05-30 17:03:39 +0100

    Create ak.js

---
+++ functions/ak.js
@@ -0,0 +1,30 @@
+export default {
+  async fetch(request, env) {
+
+    try {
+
+      cached_time = await env.CACHE.read("someCacheKey", async (key) => {
+        // time now
+        d1 = new Date()
+        // in one minute
+        d2 = new Date(d1.getTime()+60000)
+        
+        // Value is the string representation of the expiry time
+        return { value: d2.toTimeString(), expiration: d2 };
+      })
+
+      from_qs_with = await env.RULES.get("28181987:429827431:cdn:flame-test-data")
+      from_qs_without = await env.RULES.get(":cdn:flame-test-data")
+
+      body = "<html><body>\n"
+      body += "time: " + cached_time + "</p>\n"
+      body += "from_qs_with: '" + from_qs_with + "'</p>\n"
+      body += "from_qs_without: '" + from_qs_without + "'</p>\n"
+      body += "</body></html>"
+
+      return new Response(body)
+    }catch (error) {
+      return new Response(error.stack, { status: 500 })
+    }
+  }
+}
